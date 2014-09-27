e9e8eb9ee2a34af4c979093caa197519ec70bb12
Daniel Tebart <daniel.tebart@gmx.de>
2014-09-27 20:42:31 +0200

    Einfache Paket-Klasse erstellt

---
+++ NetworkLib/Paket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkLib
+{
+    public class Paket
+    {
+        private IPAddress _srcIp;
+        private IPAddress _destIp;
+        private string _content;
+
+        // ---------------------- Constructors ----------------------
+        public Paket()
+        {
+            _srcIp = IPAddress.Loopback;
+            _destIp = IPAddress.Loopback;
+            _content = "Hello World!";
+        }
+
+        public Paket(IPAddress srcIp, IPAddress destIp){
+            _srcIp = srcIp;
+            _destIp = destIp;
+            _content = String.Empty;
+        }
+
+        public Paket(IPAddress srcIp, IPAddress destIp, string content)
+            : this(srcIp, destIp)
+        {
+            _content = content;
+        }
+
+        // ---------------------- Getter/Setter ----------------------
+        public IPAddress SrcIp
+        {
+            get { return _srcIp; }
+        }
+        public IPAddress DestIp
+        {
+            get { return _destIp; }
+        }
+        public string Content
+        {
+            get { return _content; }
+        }
+        public Byte[] Bytes
+        {
+            get { return System.Text.Encoding.ASCII.GetBytes(_content); }
+        }
+    }
+}
