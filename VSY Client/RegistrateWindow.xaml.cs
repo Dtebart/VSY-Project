e4bb74590d314be7b7088643e604a3da6858fc23
Daniel Tebart <daniel.tebart@gmx.de>
2014-10-26 08:30:44 +0100

    Start/Registrieren Fenster erstellt

---
+++ VSY Client/RegistrateWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VSY_Client
+{
+    /// <summary>
+    /// Interaktionslogik für RegistrateWindow.xaml
+    /// </summary>
+    public partial class RegistrateWindow : Window
+    {
+        public RegistrateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
